b7f14b69676ddb858f06fcfe12d7a639b0c78432
xNewz <pargornruasijan@gmail.com>
2022-04-19 22:52:44 +0700

    create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_stack_binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
