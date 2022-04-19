dd400a0aa18ef83f02ebcbae9c02ff7a22aef5f9
xNewz <pargornruasijan@gmail.com>
2022-04-19 22:54:51 +0700

    add: array

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace csharp_stack_binary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] arr = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
         }
     }
 }
