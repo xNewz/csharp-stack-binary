0664f56d887f3db693843cced7d8cc935028844a
xNewz <pargornruasijan@gmail.com>
2022-04-19 22:56:00 +0700

    add: create stack

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace csharp_stack_binary
 {
@@ -7,6 +8,7 @@ namespace csharp_stack_binary
         static void Main(string[] args)
         {
             string[] arr = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+            Stack<string> s = new Stack<string>();
         }
     }
 }
